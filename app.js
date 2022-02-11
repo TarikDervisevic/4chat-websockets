01bed223d07b8800ea7da76e2089fb8bee29eab6
Tarik Dervišević <tarikov.mail@gmail.com>
2022-02-11 20:51:47 +0100

    readded SPA code

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.use("/api/boards", boardRoutes);
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"))
-  //app.get("*", (req, res) => {
-  //  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  //})
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
 }
 
 const server = http.createServer(app);
